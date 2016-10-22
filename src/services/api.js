b2e9e5fcff81480f988d1902837de0bf9c7487c5
Sam-Mauris Yong <mauris@hotmail.sg>
2016-10-23 00:24:40 +0800

    updated api to be a constant instead

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,5 @@
 const app = require('../app');
 
-app.factory('api', () => {
-  return {
-    'rootUrl': 'http://localhost:3000/v1'
-  };
+app.constant('api', {
+  'rootUrl': 'http://localhost:3000/v1'
 });
