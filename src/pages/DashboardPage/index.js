55ba5cad7a7655f5f199a0cb4e88d6a893deb39d
Sam-Mauris Yong <mauris@hotmail.sg>
2016-10-23 00:24:33 +0800

    updated dashboard to load event details

--- src/pages/DashboardPage/index.js
+++ src/pages/DashboardPage/index.js
@@ -1,7 +1,14 @@
 const app = require('../../app');
 
-let $controller = ($scope, $window, $http, $routeParams) => {
-
+let $controller = ($scope, $window, $http, $routeParams, api) => {
+  $scope.event = {};
+  let loadEvent = () => {
+    $http.get(api.rootUrl + '/event')
+      .then((res) => {
+        $scope.event = res.data.result;
+      });
+  };
+  loadEvent();
 };
 
 app.config(($routeProvider) => {
