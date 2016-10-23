e8534329004d58e3c0c46f82171c742969f0f491
Sam-Mauris Yong <mauris@hotmail.sg>
2016-10-23 17:34:37 +0800

    added loading disable for group list

--- src/components/GroupList/index.js
+++ src/components/GroupList/index.js
@@ -3,6 +3,10 @@ const app = require('../../app');
 let $controller = ($scope, $http, api, $interval, toastr) => {
   $scope.isLoading = false;
   $scope.groups = [];
+  $scope.isRevokeTicketLoading = false;
+  $scope.isStartGroupLoading = false;
+  $scope.isCallGroupLoading = false;
+  $scope.isMarkPresentLoading = false;
 
   var load = () => {
     $scope.isLoading = true;
@@ -14,29 +18,36 @@ let $controller = ($scope, $http, api, $interval, toastr) => {
   };
 
   $scope.callGroup = (group) => {
+    $scope.isCallGroupLoading = true;
     $http.post(api.rootUrl + '/groups/' + group.groupId + '/request', '', { headers: { 'Content-Type': 'text/plain' } })
       .then((result) => {
+        $scope.isCallGroupLoading = false;
         toastr.success('A message calling for group ' + group.groupId + ' was sent.');
       });
   };
 
   $scope.markGroupPresent = (group) => {
+    $scope.isMarkPresentLoading = true;
     $http.post(api.rootUrl + '/groups/' + group.groupId + '/markpresent', '', { headers: { 'Content-Type': 'text/plain' } })
       .then((result) => {
         group.isPresent = true;
         toastr.success('Group ' + group.groupId + ' was marked present. At your discretion you may let the group in.');
+        $scope.isMarkPresentLoading = false;
       });
   };
 
   $scope.startGroup = (group) => {
+    $scope.isStartGroupLoading = true;
     $http.post(api.rootUrl + '/groups/' + group.groupId + '/start', '', { headers: { 'Content-Type': 'text/plain' } })
       .then((result) => {
         $scope.groups.splice($scope.groups.indexOf(group), 1);
         toastr.success('Group ' + group.groupId + ' has started. You may send more groups to the entrance using the Call function.');
+        $scope.isStartGroupLoading = false;
       });
   };
 
   $scope.revokeTicket = (group, ticket) => {
+    $scope.isRevokeTicketLoading = true;
     $http.delete(api.rootUrl + '/tickets/' + ticket.ticketId)
       .then((res) => {
         group.totalNoOfPeople -= ticket.noOfPeople;
@@ -47,6 +58,7 @@ let $controller = ($scope, $http, api, $interval, toastr) => {
           $scope.groups.splice($scope.groups.indexOf(group), 1);
         }
         toastr.success('Ticket #' + ticket.ticketId + " from Group" + group.groupId + ' has been cancelled. ');
+        $scope.isRevokeTicketLoading = false;
       });
   };
 
